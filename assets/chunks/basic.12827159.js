4ceffa00831c88835ec7c0c9158102a0e57ea5f9
kvuse <kvuse@users.noreply.github.com>
2022-12-13 08:21:33 +0000

    deploy: aeff1b0222caf6ae79a6486816634395bda3ecc8

---
+++ assets/chunks/basic.12827159.js
@@ -0,0 +1,1 @@
+import{h as o,r,o as s,i as a}from"../app.99166b49.js";const i={__name:"basic",setup(c){const e=o([{title:"\u9996\u9875",path:"home"},{title:"\u524D\u53F0"}]);return(n,l)=>{const t=r("k-breadcrumb");return s(),a(t,{list:e.value},null,8,["list"])}}};export{i as default};
--- assets/chunks/small.a6d6cd5e.js
+++
@@ -1,1 +0,0 @@
-import{h as l,r as u,o as c,i as p}from"../app.27be988a.js";const g={__name:"small",setup(m){const t=l(50),e=l(1),n=o=>{console.log("page: ",o)};return(o,a)=>{const s=u("k-page");return c(),p(s,{total:t.value,small:"",modelValue:e.value,"onUpdate:modelValue":a[0]||(a[0]=r=>e.value=r),onCurrentChange:n},null,8,["total","modelValue"])}}};export{g as default};
