1959fdacabcb334b6b4261529462c17255b3f23a
kvuse <kvuse@users.noreply.github.com>
2023-02-15 09:21:14 +0000

    deploy: 26e243663dfd15cecd8ec0517661e05d623db3f7

--- assets/chunks/basic.1a220d73.js
+++
@@ -1,1 +0,0 @@
-import{h as o,r,o as s,i as a}from"../app.64419615.js";const i={__name:"basic",setup(c){const e=o([{title:"首页",path:"home"},{title:"前台"}]);return(n,l)=>{const t=r("k-breadcrumb");return s(),a(t,{list:e.value},null,8,["list"])}}};export{i as default};
