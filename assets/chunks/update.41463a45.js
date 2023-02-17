3b75b8101545e9b1698bfae8552785bf7b565e4e
kvuse <kvuse@users.noreply.github.com>
2023-02-17 08:44:54 +0000

    deploy: 0a9514dd617578c870ef2c5e4c4abcdc4ab16ad2

---
+++ assets/chunks/update.41463a45.js
@@ -0,0 +1,1 @@
+import{h as o,f as n,o as a,c as t,g as c,e as l,t as r,d as u}from"../app.ca965315.js";const i=u("span",null,"金额处理：",-1),d={__name:"update",setup(_){const e=o(88.88888);return setTimeout(()=>{e.value=188.66666},1e3),(p,v)=>{const s=n("money");return a(),t("span",null,[i,c((a(),t("span",null,[l(r(e.value),1)])),[[s,e.value,"value"]])])}}};export{d as default};
