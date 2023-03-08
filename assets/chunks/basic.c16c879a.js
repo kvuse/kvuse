9e5234af96d25fc76196250b4fff2ec53325829c
kvuse <kvuse@users.noreply.github.com>
2023-03-08 12:29:00 +0000

    deploy: b6b2810a03fdfb6b1c7576d6adaf7b6ce51efa93

--- assets/chunks/basic.c16c879a.js
+++
@@ -1,1 +0,0 @@
-import{h as l,r as n,o as s,c as r,b as c}from"../app.d037b9af.js";const u={class:"width-240"},m={__name:"basic",setup(p){const e=l("");return(_,o)=>{const t=n("kv-input");return s(),r("div",u,[c(t,{placeholder:"请输入金额",modelValue:e.value,"onUpdate:modelValue":o[0]||(o[0]=a=>e.value=a)},null,8,["modelValue"])])}}};export{m as default};
